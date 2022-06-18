77eb8d0fb91dadd6109e5212daa9f818c0840ddf
陈昊天 <2363822647@qq.com>
2022-06-18 22:32:05 +0800

    upd9.1 modify

---
+++ C程序课程设计/Steam++.md
@@ -0,0 +1,1 @@
+https://nme-file.oss-cn-hangzhou.aliyuncs.com/github/zstu-study/Steam%2B%2B_win_x64_v2.7.2.7z
--- C程序课程设计/课程设计定稿/data.cpp
+++ C程序课程设计/课程设计定稿/data.cpp
@@ -3,23 +3,18 @@
 #define FOR(i,a,b) for(int i=(a);i<=(b);++i)
 using namespace std;
 
-string int2str2(int num){
-    string str2;
-    str2=to_string(num);
-    return str2;
-}
 signed main(){
     cin.tie(0)->sync_with_stdio(0);
     //clock_t st=clock();
     //freopen("input.txt","r",stdin);
-    freopen("dev(1e2).in","w",stdout);
+    freopen("dev(1e5).in","w",stdout);
     int id,prc,amt;
     string ids,prcs,nme,rcp,dpt,amts,dat;
     srand(time(0));
-    int n=2e5;
+    const int n=1e5;
     FOR(i,1,n){
         id=rand()%100000+1;
-        ids=int2str2(id);
+        ids=to_string(id);
         int lenids=ids.size();
         if(lenids<7){
             FOR(i,1,7-lenids) ids="0"+ids;
@@ -43,12 +38,12 @@ signed main(){
             dpt+=c;
         }
         dat="";
-        dat+=int2str2(rand()%70+1950)+"."+int2str2(rand()%12+1)+"."+int2str2(rand()%29+1);
+        dat+=to_string(rand()%70+1950)+"."+to_string(rand()%12+1)+"."+to_string(rand()%29+1);
         cout<<ids<<" "<<nme<<" "<<rcp<<" "<<dpt<<" "<<amt<<" "<<dat<<" "<<prc<<endl;
 
     }
-    //fclose(stdout);
-    clock_t ed=clock();
+    fclose(stdout);
+    //clock_t ed=clock();
     //cout<<ed-st;
     return 0;
 }
