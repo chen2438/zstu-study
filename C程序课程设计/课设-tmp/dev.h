9d4b4ca6e2d5d400ab10c3a28334df9825d107cf
陈昊天 <2363822647@qq.com>
2022-06-18 15:14:44 +0800

    upd7 v1.4

--- C程序课程设计/课设-tmp/dev.h
+++ C程序课程设计/课设-tmp/dev.h
@@ -1,49 +1,56 @@
-#pragma GCC optimize(2)
-#include<iostream>
-#include<cstdio>
-#include<cmath>
-#include<cstring>
-#include<ctime>
-#include<fstream>
-#include<vector>
-#include<algorithm>
-#include<windows.h>
-#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+//编译指示
+#pragma GCC optimize(2)//开启O(2)优化
 
-using namespace std;
+//头文件包含
+#include<cstdio>//基本输入输出函数库
+#include<iostream>//输入输出流函数库
+#include<string>//字符串函数库
+#include<ctime>//时间函数库
+#include<fstream>//文件流函数库
+#include<vector>//vector函数库
+#include<algorithm>//算法函数库
+#include<windows.h>//windows函数库
 
+//宏定义
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)//宏定义for循环
+
+//使用命名空间
+using namespace std;//使用标准命名空间
+
+//设备信息结构体
 struct device{
 	//按顺序为 存储序列号、设备编号、设备名称、领用人、所属部门、数量、购买时间、价格
 	int num;
 	string id,nme,rcp,dpt,sum,tme,prc;
 };
 
-const string obj[10]={"存储序列号","设备编号","设备名称","领用人","所属部门","数量","购买时间","价格"};
-//const string InFile="dev(1e1).in";
-vector<device> dev;
-vector<vector<device>> backup;
+const string obj[10]={"存储序列号","设备编号","设备名称","领用人","所属部门","数量","购买时间","价格"};//定义信息名称
 
-void ShowMenu();
-void ImportDev();
-void ReturnMenu();
-void SaveDev(string file,vector<device> save,string obj);
-void ViewDev();
-void QueryDev();
-void DeletDev();
-bool CmpStr(string x1,string x2);
-bool CmpStr(string x1,string x2,string x3);
-bool CmpTme(string x1,string x2);
-bool CmpTme(string x1,string x2,string x3);
-void sort(vector<device>::iterator st, vector<device>::iterator ed,int op);
-void SortDev();
-void FilterDev();
-void UndoDev();
+vector<device> dev;//定义设备信息
+vector<vector<device>> backup;//定义二维数组，设备信息备份，用于撤销
+
+void ShowMenu();//人机交互主界面
+void ReturnMenu();//返回主界面
+void PutDev(device T);//打印设备信息
+void SaveDev(string file,vector<device> save,string obj);//保存设备信息
+void ImportDev();//导入或添加设备
+void ViewDev();//信息浏览
+void QueryDev();//信息查询
+void DeletDev();//信息删除
+bool CmpStr(string x1,string x2);//对string以数字大小排序
+bool CmpStr(string x1,string x2,string x3);//判断区间
+bool CmpTme(string x1,string x2);//对购买时间排序
+bool CmpTme(string x1,string x2,string x3);//判断区间
+void sort(vector<device>::iterator st, vector<device>::iterator ed,int op);//重载sort函数
+void SortDev();//信息排序
+void FilterDev();//信息筛选
+void UndoDev();//撤销操作
 
 void ShowMenu(){//人机交互主界面
 	putchar('\n');
 	cout<<"*********************\n";
 	cout<<"*  0-退出系统       *\n";
-	cout<<"*  1-信息导入与添加 *\n";
+	cout<<"*  1-信息录入       *\n";
 	cout<<"*  2-信息浏览       *\n";
 	cout<<"*  3-信息查询       *\n";
 	cout<<"*  4-删除设备       *\n";
@@ -70,47 +77,43 @@ void ShowMenu(){//人机交互主界面
 	}
 }
 
-void ReturnMenu(){
+void ReturnMenu(){//返回主界面
 	cout<<"操作完成, 1 秒后返回初始界面.\n";
 	Sleep(1000);
 	ShowMenu();
 }
 
 void PutDev(device T){//打印设备信息
-	//cout.setf(ios::left);
-    //cout.width(20);
-	cout<<obj[0]+"："<<T.num;
-	cout<<" "+obj[1]+"："<<T.id;
-	cout<<" "+obj[2]+"："<<T.nme;
-	cout<<" "+obj[3]+"："<<T.rcp;
-	cout<<" "+obj[4]+"："<<T.dpt;
-	cout<<" "+obj[5]+"："<<T.sum;
-	cout<<" "+obj[6]+"："<<T.tme;
-	cout<<" "+obj[7]+"："<<T.prc<<endl;
-	//cout.unsetf(ios::left);
+	cout<<obj[0]+':'<<T.num;
+	cout<<' '+obj[1]+':'<<T.id;
+	cout<<' '+obj[2]+':'<<T.nme;
+	cout<<' '+obj[3]+':'<<T.rcp;
+	cout<<' '+obj[4]+':'<<T.dpt;
+	cout<<' '+obj[5]+':'<<T.sum;
+	cout<<' '+obj[6]+':'<<T.tme;
+	cout<<' '+obj[7]+':'<<T.prc<<endl;
 }
 
 void SaveDev(string file,vector<device> save,string obj){//保存设备信息
-	clock_t st=clock();
+	clock_t st=clock();//记录起始时间
 	if(obj=="dev"){
-		backup.push_back(save);
+		backup.push_back(save);//备份此次操作
 		if(backup.size()==3) backup.erase(backup.begin());
 	}
 	ofstream ofs;
-	ofs.open(file,ios::out | ios::trunc);
+	ofs.open(file,ios::out | ios::trunc);//打开写入文件
 	if(!ofs.is_open()) {cout<<"写入文件打开失败！"<<endl; exit(0);}
 	int len=save.size();
 	#define T save[i]
 	FOR(i,0,len-1){
-		save[i].num=i+1;
-		//auto T=save[i];
+		save[i].num=i+1;//重排序列号
 		ofs<<T.num<<' '<<T.id<<' '<<T.nme<<' '<<T.rcp<<' '<<T.dpt<<\
 			' '<<T.sum<<' '<<T.tme<<' '<<T.prc<<'\n';
 	}
 	#undef T
-	ofs.close();
+	ofs.close();//关闭文件
 	if(obj=="dev") dev=save;
-	clock_t ed=clock();
+	clock_t ed=clock();//记录结束时间
 	cout<<file<<" 已保存更改, 耗时 "<<ed-st<<" 毫秒\n";
 	cout<<file<<" 内目前有 "<<save.size()<<" 条记录\n";
 }
@@ -131,22 +134,20 @@ void ImportDev(){//导入或添加设备
 	if(op=="1"){
 		cout<<"请输入文件名:";
 		string InFile;cin>>InFile;
-		clock_t st=clock();
+		clock_t st=clock();//起始时间
 		ifstream ifs;
-		ifs.open(InFile, ios::in);
-		//1e3 10ms 1e4 72ms 1e5 696ms 1e6 6766ms
+		ifs.open(InFile, ios::in);//打开文件
 		if(!ifs.is_open()) {cout<<"读取文件打开失败\n"; exit(0);}
 		device T;
 		while(ifs>>T.id>>T.nme>>T.rcp>>T.dpt>>T.sum>>T.tme>>T.prc){
-			dev.push_back(T);
+			dev.push_back(T);//读取信息
 		}
-		ifs.close();
-		clock_t ed=clock();
+		ifs.close();//关闭文件
+		clock_t ed=clock();//结束时间
 		cout<<"读取完成, 耗时 "<<ed-st<<" 毫秒\n";
 	}
 	if(op=="2"){
 		device T;
-		T.num=dev.size()+1;
 		cout<<"请输入"+obj[1]+":"; cin>>T.id;
 		cout<<"请输入"+obj[2]+":"; cin>>T.nme;
 		cout<<"请输入"+obj[3]+":"; cin>>T.rcp;
@@ -156,7 +157,7 @@ void ImportDev(){//导入或添加设备
 		cout<<"请输入"+obj[7]+":"; cin>>T.prc;
 		dev.push_back(T);
 	}
-	SaveDev("dev.out",dev,"dev");
+	SaveDev("dev.out",dev,"dev");//保存信息
 	ReturnMenu();
 }
 
@@ -183,23 +184,23 @@ void QueryDev(){//信息查询
 		cin>>op;
 	}
 	clock_t st=clock();
-	vector<device> res;
+	vector<device> res;//存储查询结果
 	if(op=="1"){
 		cout<<"请输入设备名称:";
 		string nme;cin>>nme;
-		st=clock();
+		st=clock();//起始时间
 		for(auto T:dev) if(T.nme==nme) res.push_back(T);
 	}
 	if(op=="2"){
 		cout<<"请输入所属部门名称:";
 		string dpt;cin>>dpt;
-		st=clock();
+		st=clock();//起始时间
 		for(auto T:dev) if(T.dpt==dpt) res.push_back(T);
 	}
 	if(op=="3"){
 		cout<<"请输入关键字:";
 		string x;cin>>x;
-		st=clock();
+		st=clock();//起始时间
 		for(auto T:dev){
 			if(T.id==x or T.nme==x or T.rcp==x or T.dpt==x \
 				or T.sum==x or T.tme==x or T.prc==x){
@@ -207,7 +208,7 @@ void QueryDev(){//信息查询
 			}
 		}
 	}
-	clock_t ed=clock();
+	clock_t ed=clock();//结束时间
 	if(res.size()>0){
 		cout<<"查询到 "<<res.size()<<" 条记录, 耗时 "<<ed-st<<" 毫秒\n";
 		putchar('\n');
@@ -249,17 +250,17 @@ void DeletDev(){//信息删除
 			cout<<"非法序列号，请重新输入:";
 			cin>>num;
 		}
-		dev.erase(dev.begin()+atoi(num.c_str())-1);
+		dev.erase(dev.begin()+atoi(num.c_str())-1);//删除记录
 	}
 	if(op=="2"){
-		dev.clear();
+		dev.clear();//清空记录，不释放内存
 		cout<<"已清空数据\n";
 	}
 	SaveDev("dev.out",dev,"dev");
 	ReturnMenu();
 }
 
-bool CmpStr(string x1,string x2){//对string以数字大小排序,而非字典序
+bool CmpStr(string x1,string x2){//对string以数字大小排序
 	if(x1==x2) return false;
 	else if(x1.length()!=x2.length()) return x1.length()<x2.length();
 	else return x1<x2;
@@ -287,8 +288,8 @@ bool CmpTme(string x1,string x2,string x3){//判断区间
 	else return (CmpTme(x1,x2) and CmpTme(x2,x3));
 }
 
-void sort(vector<device>::iterator st, vector<device>::iterator ed,int op){
-    switch(op){
+void sort(vector<device>::iterator st, vector<device>::iterator ed,int op){//重载sort函数
+    switch(op){//使用lambda函数重载cmp函数
 		case 1: sort(st,ed,[](device x,device y){return CmpStr(x.id,y.id);});break;
 		case 2: sort(st,ed,[](device x,device y){return CmpStr(x.sum,y.sum);});break;
 		case 3: sort(st,ed,[](device x,device y){return CmpTme(x.tme,y.tme);});break;
@@ -300,7 +301,7 @@ void sort(vector<device>::iterator st, vector<device>::iterator ed,int op){
 	}
 }
 
-void SortDev(){//排序设备信息
+void SortDev(){//信息排序
 	putchar('\n');
 	cout<<"*******************\n";
 	cout<<"*  1-按设备编号升序 *\n";
@@ -319,14 +320,13 @@ void SortDev(){//排序设备信息
 		cout<<"->请选择操作:";
 		cin>>op;
 	}
-	clock_t st=clock();
+	clock_t st=clock();//起始时间
 	sort(dev.begin(),dev.end(),atoi(op.c_str()));
-	clock_t ed=clock();
+	clock_t ed=clock();//结束时间
 	cout<<"排序完成, 耗时 "<<ed-st<<" 毫秒\n";
 	SaveDev("dev.out",dev,"dev");
 	cout<<"是否浏览数据?(y/n)\n";
-	char c;
-	cin>>c;
+	char c;cin>>c;
 	while(!(c=='y' or c=='n')){
 		cout<<"非法输入，请重新输入:";
 		cin>>c;
@@ -335,7 +335,7 @@ void SortDev(){//排序设备信息
 	if(c=='n') ReturnMenu();
 }
 
-void FilterDev(){//筛选设备信息
+void FilterDev(){//信息筛选
 	putchar('\n');
 	cout<<"*******************\n";
 	cout<<"*  1-按设备数量筛选 *\n";
@@ -349,28 +349,28 @@ void FilterDev(){//筛选设备信息
 		cout<<"->请选择操作:";
 		cin>>op;
 	}
-	vector<device> res;
+	vector<device> res;//存储筛选结果
 	string l,r;
 	clock_t st=clock();
 	if(op=="1"){
 		cout<<"请输入筛选范围(例:666 2333):";
 		cin>>l>>r;
-		st=clock();
+		st=clock();//起始时间
 		for(auto T:dev) if(CmpStr(l,T.sum,r)) res.push_back(T);
 	}
 	if(op=="2"){
 		cout<<"请输入筛选范围(例:2042.1.1 2077.12.31):";
 		cin>>l>>r;
-		st=clock();
+		st=clock();//起始时间
 		for(auto T:dev) if(CmpTme(l,T.tme,r)) res.push_back(T);
 	}
 	if(op=="3"){
 		cout<<"请输入筛选范围(例:6666 23333):";
 		cin>>l>>r;
-		st=clock();
+		st=clock();//起始时间
 		for(auto T:dev) if(CmpStr(l,T.prc,r)) res.push_back(T);
 	}
-	clock_t ed=clock();
+	clock_t ed=clock();//结束时间
 	if(res.size()>0){
 		cout<<"筛选到 "<<res.size()<<" 条记录, 耗时 "<<ed-st<<" 毫秒\n";
 		putchar('\n');
@@ -388,13 +388,13 @@ void FilterDev(){//筛选设备信息
 		if(op=="1") {for(auto T:res) PutDev(T);}
 		if(op=="2") {SaveDev("filter.out",res,"res");}
 	}
-	else cout<<"无法查询到此设备.\n";
+	else cout<<"无法筛选到此设备.\n";
 	ReturnMenu();
 }
 
 void UndoDev(){//撤销操作
 	putchar('\n');
-	if(backup.size()==1) dev.clear();
+	if(backup.size()==1) dev.clear();//只有一次操作,dev清空
 	else if(backup.size()==2) swap(dev,backup[0]);
 	cout<<"已撤销上次操作\n";
 	SaveDev("dev.out",dev,"dev");
