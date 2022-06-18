55b25bef1354dbad63a5dcd12dfea79456fa9a83
陈昊天 <2363822647@qq.com>
2022-06-18 21:32:08 +0800

    upd9 v1.6 end1.1

--- C程序课程设计/课程设计定稿/main.cpp
+++ C程序课程设计/课程设计定稿/main.cpp
@@ -1,15 +1,22 @@
 /*
 	# 程序最初于 Jun 12, 2022 由 chen2438 提交在 github.com/chen2438/zstu-study/
-	# 当前版本 upd8 v1.5 end1.0
-	# 提交时间 2022.6.18 21:23
+	# 当前版本 upd9 v1.6 end1.1
+	# 提交时间 2022.6.18 21:31
 	# 遵循 GNU General Public License v3.0 协议
 */
 
 #pragma GCC optimize(2)//开启O(2)优化
+//#define debug
 #include "dev.h"
 
 int main(){
-	//ShowMenu();//人机交互主界面
-	for(int i=0;i<10;i++)ImportTest("dev(1e6).in");
+	#ifndef debug
+	ShowMenu();//人机交互主界面
+	#endif
+
+	#ifdef debug
+	for(int i=0;i<10;i++)ImportTest("dev(1e6).in");//测试
+	#endif
+
 	return 0;
 }
