4316c81bcc20b52d4b625923150bafc34b630233
Meson <134292631+zyrzjyzxy@users.noreply.github.com>
2024-10-21 17:30:58 +0800

    update 10-21

---
+++ 04专业基础课-专业课/02实践必修课/C程序课程设计/课程设计定稿/data.cpp
@@ -0,0 +1,49 @@
+#pragma GCC optimize(2)
+#include<bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+using namespace std;
+
+signed main(){//用于生成数据
+    cin.tie(0)->sync_with_stdio(0);
+    //clock_t st=clock();
+    //freopen("input.txt","r",stdin);
+    freopen("dev(1e5).in","w",stdout);
+    const int n=1e5;
+    int id,prc,amt;
+    string ids,prcs,nme,rcp,dpt,amts,dat;
+    srand(time(0));
+    FOR(i,1,n){
+        id=rand()%100000+1;
+        ids=to_string(id);
+        int lenids=ids.size();
+        if(lenids<7){
+            FOR(i,1,7-lenids) ids="0"+ids;
+        }
+        prc=rand()%10000+1000;
+        amt=rand()%500+10;
+        nme=rcp=dpt="";
+        FOR(i,1,7){
+            int rs=rand()%24;
+            char c=rs+'a';
+            nme+=c;
+        }
+        FOR(i,1,7){
+            int rs=rand()%24;
+            char c=rs+'a';
+            rcp+=c;
+        }
+        FOR(i,1,7){
+            int rs=rand()%24;
+            char c=rs+'a';
+            dpt+=c;
+        }
+        dat="";
+        dat+=to_string(rand()%70+1950)+"."+to_string(rand()%12+1)+"."+to_string(rand()%29+1);
+        cout<<ids<<" "<<nme<<" "<<rcp<<" "<<dpt<<" "<<amt<<" "<<dat<<" "<<prc<<endl;
+
+    }
+    fclose(stdout);
+    //clock_t ed=clock();
+    //cout<<ed-st;
+    return 0;
+}
